30967288d529cc12ae71d320a821e6066b10ff2f
noah-gilgo <74427747+noah-gilgo@users.noreply.github.com>
2025-10-07 21:48:52 -0400

    Implemented Frame composition object to store time data associated with each sprite. Modified graphics methods to use this new object.

--- graphics_methods.py
+++ graphics_methods.py
@@ -1,6 +1,7 @@
 import arcade
 import pyglet
 import math
+from graphics_objects import Frame
 
 DEPTHS_ANIMATION_INITIAL_SCALE = 7.0
 DEPTHS_ANIMATION_SCALE_INCREMENT = 0.05
@@ -13,10 +14,11 @@ DEPTHS_ANIMATION_COEFFICIENT = 2 * (DEPTHS_ANIMATION_MAX_ALPHA / DEPTHS_ANIMATIO
 # Maybe it would be cleaner to create a "Frame" object to bundle together sprites with their age in seconds?
 # Will probably have to do that. Maybe. I wonder if the arcade library already has an object like that.
 depths_age_of_each_frame_array = []
+# = []
 
 
-def add_sprite_to_depths_array(dt, sprite_array: list[arcade.Sprite], center, sprite_list):
-    if len(sprite_array) < DEPTHS_ANIMATION_NUMBER_OF_SPRITES:
+def add_sprite_to_depths_array(dt, frame_array: list[Frame], center, sprite_list):
+    if len(frame_array) < DEPTHS_ANIMATION_NUMBER_OF_SPRITES:
         sprite = arcade.Sprite(path_or_texture="assets/textures/backgrounds/IMAGE_DEPTHS_lowres.png",
                                center_x=center[0],
                                center_y=center[1],
@@ -24,44 +26,45 @@ def add_sprite_to_depths_array(dt, sprite_array: list[arcade.Sprite], center, sp
         sprite.alpha = 1
         sprite.scale_x = DEPTHS_ANIMATION_INITIAL_SCALE
         sprite.scale_y = DEPTHS_ANIMATION_INITIAL_SCALE
-        sprite_array.append(sprite)
+        frame = Frame(sprite)
+        frame_array.append(frame)
         sprite_list.append(sprite)
         depths_age_of_each_frame_array.append(0)
         print("sprite added to array")
 
 
-def initialize_depths_array(sprite_array, time_interval, center, sprite_list):
+def initialize_depths_array(frame_array, time_interval, center, sprite_list):
     pyglet.clock.schedule_interval(
-        lambda dt: add_sprite_to_depths_array(dt, sprite_array, center, sprite_list),
+        lambda dt: add_sprite_to_depths_array(dt, frame_array, center, sprite_list),
         time_interval
     )
 
 
-def animate_depths_frames(dt, sprite_array: list[arcade.Sprite]):
+def animate_depths_frames(dt, frame_array: list[arcade.Sprite]):
     # For every sprite in the frame:
     # 1. If alpha = 0, reset the alpha to 1 and the scale to 1.0.
-    sprite_array_index = 0
-    depths_age_of_each_frame_array_length = len(depths_age_of_each_frame_array)
-    for sprite in sprite_array:
+    frame_array_index = 0
+    frame_array_length = len(frame_array)
+    for frame in frame_array:
+        sprite = frame.sprite
         if sprite.alpha <= 0:
             sprite.scale_x = DEPTHS_ANIMATION_INITIAL_SCALE
             sprite.scale_y = DEPTHS_ANIMATION_INITIAL_SCALE
             sprite.alpha = 1
-            if depths_age_of_each_frame_array_length > 0:
-                depths_age_of_each_frame_array[sprite_array_index] = 0
+            if frame_array_length > 0:
+                frame.reset_age()
             print("alpha reached zero")
         else:
-            time = depths_age_of_each_frame_array[sprite_array_index] + dt
+            if frame_array_length > 0:
+                frame.age += dt
             sprite.scale_x += DEPTHS_ANIMATION_SCALE_INCREMENT
             sprite.scale_y += DEPTHS_ANIMATION_SCALE_INCREMENT
-            sprite.alpha = -abs(math.ceil((DEPTHS_ANIMATION_COEFFICIENT * time) - DEPTHS_ANIMATION_MAX_ALPHA)) + DEPTHS_ANIMATION_MAX_ALPHA
-            if depths_age_of_each_frame_array_length > 0:
-                depths_age_of_each_frame_array[sprite_array_index] = time
-        sprite_array_index += 1
+            sprite.alpha = -abs(math.ceil((DEPTHS_ANIMATION_COEFFICIENT * frame.age) - DEPTHS_ANIMATION_MAX_ALPHA)) + DEPTHS_ANIMATION_MAX_ALPHA
+        frame_array_index += 1
 
 
-def animate_depths(sprite_array, time_interval):
+def animate_depths(frame_array, time_interval):
     pyglet.clock.schedule_interval(
-        lambda dt: animate_depths_frames(dt, sprite_array),
+        lambda dt: animate_depths_frames(dt, frame_array),
         time_interval
     )
