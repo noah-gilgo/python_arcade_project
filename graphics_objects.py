f61f5cdee4bf127c38a404d6f18913344437314d
noah-gilgo <74427747+noah-gilgo@users.noreply.github.com>
2025-10-16 21:49:32 -0400

    Added SimpleLoopAnimation class to graphics_objects.py.

--- graphics_objects.py
+++ graphics_objects.py
@@ -11,3 +11,59 @@ class Frame:
 
     def reset_age(self):
         self.age = 0.0
+
+
+class SimpleLoopAnimation:
+    """
+    Simple class used for storing data needed for simple looping animations with a constant yet adjustable time
+    interval between frames.
+    """
+    def __init__(self, sprite_pack_path: str, frame_duration: float, loop_animation: bool):
+        self._sprite_pack_path = sprite_pack_path
+        self._frame_duration = frame_duration
+        self._loop_animation = loop_animation
+
+    def get_sprite_pack_path(self):
+        """
+        Gets the sprite path on the animation.
+        :return str: The file path of the folder containing all of the frames of the animation.
+        """
+        return self._sprite_pack_path
+
+    def set_sprite_pack_path(self, sprite_pack_path: str):
+        """
+        Sets the sprite path on the animation.
+        :param sprite_pack_path:
+        :return: None
+        """
+        self._sprite_pack_path = sprite_pack_path
+
+    def get_frame_duration(self):
+        """
+        Gets the frame duration on the animation.
+        :return: The frame duration of the animation in seconds.
+        """
+        return self._frame_duration
+
+    def set_frame_duration(self, frame_duration: float):
+        """
+        Sets the frame duration on the animation.
+        :param frame_duration: The frame duration of the animation in seconds.
+        :return: None
+        """
+        self._frame_duration = frame_duration
+
+    def get_loop_animation(self):
+        """
+        Gets whether or not the animation is meant to loop.
+        :return: A bool representing whether or not the animation is meant to loop.
+        """
+        return self._loop_animation
+
+    def set_loop_animation(self, loop_animation: bool):
+        """
+        Sets whether or not the animation is meant to loop.
+        :param loop_animation: A bool representing whether or not the animation is meant to loop.
+        :return: None
+        """
+        self._loop_animation = loop_animation
