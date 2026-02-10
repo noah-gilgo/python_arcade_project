e73d3a32cb3ecd74a41876cf33edc705a8d3e798
noah-gilgo <74427747+noah-gilgo@users.noreply.github.com>
2026-02-09 19:54:42 -0500

    Implemented elemental pair system through the ElementalPair object.

---
+++ spells.py
@@ -0,0 +1,108 @@
+from character import Character
+from non_player_character import NonPlayerCharacter
+from player_character import PlayerCharacter
+
+
+class ElementalPair:
+    """ Allows the user to assign an "element" to certain spells/attacks. """
+    def __init__(self, element_id: int = 0, name: str = "None", resistant_to: list[int] = None,
+                 weak_to: list[int] = None):
+        self.element_id = element_id
+        self.name = name
+        self.resistant_to = resistant_to
+        self.weak_to = weak_to
+
+
+def generate_elemental_pairs():
+    """
+    Generates the default elemental pairs used in Deltarune.
+    NOTE: some of these are non-canonical
+    """
+    return [
+        ElementalPair(
+            element_id=1,
+            name="Holy/Electric",
+            resistant_to=[1, 5, 7],
+        ),
+        ElementalPair(
+            element_id=2,
+            name="Life/Earth",
+            resistant_to=[1, 2, 3],
+            weak_to=[7, 8, 9]
+        ),
+        ElementalPair(
+            element_id=3,
+            name="Water/Night",
+            resistant_to=[3, 8, 6],
+            weak_to=[1]
+        ),
+        ElementalPair(
+            element_id=4,
+            name="Bird/Wind",
+            resistant_to=[2, 4, 9],
+            weak_to=[1, 7]
+        ),
+        ElementalPair(
+            element_id=5,
+            name="Dark/Star",
+            resistant_to=[3, 5],
+            weak_to=[1]
+        ),
+        ElementalPair(
+            element_id=5,
+            name="Dark/Star",
+            resistant_to=[5],
+            weak_to=[1, 9]
+        ),
+        ElementalPair(
+            element_id=6,
+            name="Puppet/Cat",
+            resistant_to=[6],
+            weak_to=[3]
+        ),
+        ElementalPair(
+            element_id=7,
+            name="System/Mouse",
+            resistant_to=[5, 6, 7],
+            weak_to=[1, 8]
+        ),
+        ElementalPair(
+            element_id=8,
+            name="Fire/Ice",
+            resistant_to=[8],
+            weak_to=[3]
+        ),
+        ElementalPair(
+            element_id=9,
+            name="Death/Scythe",
+            resistant_to=[2, 8, 9],
+            weak_to=[10]
+        ),
+        ElementalPair(
+            element_id=10,
+            name="Hopes/Dreams",
+            resistant_to=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        )
+    ]
+
+
+class Spell:
+    """ Parent class for spells """
+    def __init__(self, tp_cost: int, element: ElementalPair):
+        self.tp_cost = tp_cost
+        self.element = element
+
+    def cast(self):
+        """ Template for the function for casting spells. """
+
+
+"""
+class PartySpell(Spell):
+    def __init__(self, tp_cost: int, targets):
+        super().__init__(tp_cost, targets)
+
+
+class EnemySpell(Spell):
+    def __init__(self, tp_cost: int, targets):
+        super().__init__(tp_cost, targets)
+"""
